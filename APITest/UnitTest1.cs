c27fc680540ca4c95a6489755af2e6ea3de5c58d
QuocBaoKho <113084141+QuocBaoKho@users.noreply.github.com>
2023-11-11 15:54:12 +0700

    Update UnitTest1.cs

--- APITest/UnitTest1.cs
+++ APITest/UnitTest1.cs
@@ -1,12 +1,21 @@
-using FirebaseAPIProject;
+using FirebaseAPIProject.Controllers;
 namespace APITest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UserRetrievalTest()
         {
+            UsersController usersController = new UsersController();
+            Assert.IsNotNull(usersController.GetUsers(), "It's not null");
+
+        }
+        [TestMethod]
+        public void PostRetrievalTest() {
+            PostsController postsController = new PostsController();
+            Assert.IsNotNull(postsController.GetUsers(), "YAH YAH");
+        
         }
     }
 }
