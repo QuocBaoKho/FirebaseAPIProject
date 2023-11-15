577d0a8d5e30f70109f21bb09b1cee1cb0e2e356
Trinh Minh Quan <121121741+q113pt@users.noreply.github.com>
2023-11-16 02:02:03 +0700

    Add UserReportService

---
+++ FirebaseAPIProject/Services/UserReportService.cs
@@ -0,0 +1,30 @@
+﻿using Firebase.Database;
+using FirebaseAPIProject.Models;
+using Firebase.Database.Query;
+
+namespace FirebaseAPIProject.Services
+{
+    public class UserReportService
+    {
+        private string dbURL = FirebaseLink.link;
+        private readonly FirebaseClient client;
+
+        public UserReportService()
+        {
+            client = new FirebaseClient(dbURL);
+        }
+        public UserReportService(FirebaseClient client)
+        {
+            this.client = client;
+        }
+        public async Task<FirebaseObject<UserReport>> addUserReport(UserReport userReport)
+        {
+            return await client.Child("Report").Child("User").PostAsync(userReport);
+        }
+        public async Task<List<KeyValuePair<string, UserReport>>> extractData()
+        {
+            var userReport = await client.Child("Report").Child("User").OnceAsync<UserReport>();
+            return userReport?.Select(x => new KeyValuePair<string, UserReport>(x.Key, x.Object)).ToList();
+        }
+    }
+}
