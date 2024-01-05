471cd055b744d781b2293963a03ea3d8435fda29
QuocBaoKho <113084141+QuocBaoKho@users.noreply.github.com>
2024-01-05 10:05:54 +0700

    Update Post.cs

--- FirebaseAPIProject/Models/Post.cs
+++ FirebaseAPIProject/Models/Post.cs
@@ -26,6 +26,10 @@
                 return false;
             return this.Postid.Equals(postItem.Postid);
         }
+        public override int GetHashCode()
+        {
+            return this.Postid.GetHashCode();
+        }
        
     }
 }
